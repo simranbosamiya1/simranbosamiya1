1897e5de8f9c5f36dfcd2671c1f1d7b409f5611d
simranbosamiya1 <89114006+simranbosamiya1@users.noreply.github.com>
2021-08-17 21:36:03 -0400

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,1 +1,13 @@
 
+
+def sort(l):
+    for i in range(0, len(l)):
+        x = l[i]
+        j = i - 1
+        while(j>=0 and l[j]>x):
+            l[j+1] = l[j]
+            j = j - 1
+        l[j+1] = x
+    return l
+    
+print(sort([1,4,5,3,2]))
