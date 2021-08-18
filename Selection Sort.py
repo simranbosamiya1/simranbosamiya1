844aab53c179975cbf6802dc0763c8ecc7eb967d
simranbosamiya1 <89114006+simranbosamiya1@users.noreply.github.com>
2021-08-18 18:42:54 -0400

    Update Selection Sort.py

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,1 +1,16 @@
 
+
+def sort(l):
+    minimum = l[0]
+    
+    for j in range(0,len(l)-1):
+        minimum = l[j]
+        for i in range(j, len(l)):
+            if(l[i]<minimum):
+                minimum = l[i]
+        l.remove(minimum)
+        l = l[:j] + [minimum] + l[j:]
+        
+    return l
+    
+print(sort([1,4,5,3,2]))
